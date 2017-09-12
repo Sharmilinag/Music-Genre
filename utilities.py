76dd527053dcc48530e29e5ca14f580a06dd4231
sharmilinag <sn469@njit.edu>
2017-09-12 00:00:00 -0400

    utilities.py

--- utilities.py
+++ utilities.py
@@ -115,4 +115,3 @@ def plot_roc_curves(auc_score, name, tpr, fpr, label=None):
     filename = name.replace(" ", "_")
     pylab.savefig(os.path.join(CHART_DIR, "roc_" + filename + ".png"), bbox_inches="tight")
 
-#^^^()()^^^
